4d833d024b94f589390647d49fa84d8ed0dce6d1
Laszlo Bugyinszki <bugyinszkilaszlo@gmail.com>
2026-01-22 11:44:25 +0100

    routes

---
+++ src/components/Nav.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li> 
+      <li><router-link to="/recipes">Recipes</router-link></li> 
+      <li><router-link to="/planner">Planner</router-link></li> 
+    </ul>
+  </nav>  
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Recipes from '@/views/Recipes.vue'
+import Planner from '@/views/Planner.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/recipes',
+      name: 'recipes',
+      component: Recipes,
+    },
+    {
+      path: '/planner',
+      name: 'planner',
+      component: Planner,
+    }
+  ],
 })
 
 export default router
